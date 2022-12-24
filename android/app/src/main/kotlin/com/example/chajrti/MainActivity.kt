56f8aaafb50d9696f1399e84f4ef4adab0aea30f
Your Name <you@example.com>
2022-12-24 01:23:45 +0100

    second commit

---
+++ android/app/src/main/kotlin/com/example/chajrti/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chajrti
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Constants/Colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+Color mainGreen = Color(0xff00703C);
+Color mediumGreen = Color(0xff80B89E);
+Color lightGreen = Color(0xffC0DCCF);
+Color grey = Color(0xffEFEFEF);
