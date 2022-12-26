0196a05891b6dc4f1af6933aa441c559ebeb7825
Idris Saddi <saddi.idris@gmail.com>
2022-12-26 21:52:00 +0100

    creating the log in screen

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -1,4 +1,6 @@
-import 'package:chajrti/Widgets/BottomBar.dart';
+// ignore_for_file: prefer_const_constructors, sort_child_properties_last
+
+import 'package:chajrti/Constants/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
@@ -13,13 +15,156 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
+    String defaultFontFamily = 'Roboto-Light.ttf';
+    double defaultFontSize = 14;
+    double defaultIconSize = 17;
     return Scaffold(
-      body:Container(
-        child: const Center(
-          child: Text("Login Page"),
+      body: Container(
+        padding: EdgeInsets.only(left: 20, right: 20, top: 35, bottom: 30),
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.white70,
+        child: Column(
+          children: <Widget>[
+            Flexible(
+              flex: 5,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 200,
+                    height: 200,
+                    alignment: Alignment.center,
+                    child: Image.asset("assets/connexionImage.png"),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  TextField(
+                    showCursor: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      filled: true,
+                      prefixIcon: Icon(
+                        Icons.email_outlined,
+                        color: mainGreen,
+                        size: defaultIconSize,
+                      ),
+                      fillColor: lighterGreen,
+                      hintStyle: TextStyle(
+                          color: mainGreen,
+                          fontFamily: defaultFontFamily,
+                          fontSize: defaultFontSize),
+                      hintText: "Email",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  TextField(
+                    showCursor: true,
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                        borderSide: BorderSide(
+                          width: 0,
+                          style: BorderStyle.none,
+                        ),
+                      ),
+                      filled: true,
+                      prefixIcon: Icon(
+                        Icons.lock_outline,
+                        color: mainGreen,
+                        size: defaultIconSize,
+                      ),
+                      suffixIcon: Icon(
+                        Icons.remove_red_eye,
+                        color: mainGreen,
+                        size: defaultIconSize,
+                      ),
+                      fillColor: lighterGreen,
+                      hintStyle: TextStyle(
+                        color: mainGreen,
+                        fontFamily: defaultFontFamily,
+                        fontSize: defaultFontSize,
+                      ),
+                      hintText: "Password",
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  //add checkbox
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          Checkbox(
+                            value: false,
+                            onChanged: (value) {},
+                            activeColor: mainGreen,
+                          ),
+                          Text(
+                            "Je suis un Commerçant",
+                            style: TextStyle(
+                              color: mainGreen,
+                              fontSize: 14,
+                              fontFamily: 'Poppins-Medium.ttf',
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Container(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      //add padding to the button
+                      style: ButtonStyle(
+                        padding: MaterialStateProperty.all<EdgeInsets>(
+                            EdgeInsets.all(17.0)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                            side: BorderSide(color: mainGreen),
+                          ),
+                        ),
+                        backgroundColor:
+                            MaterialStateProperty.all<Color>(mainGreen),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        "Connexion",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontFamily: 'Poppins-Medium.ttf',
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
-      bottomNavigationBar: BottomBar(isClient: true,),
     );
   }
 }
