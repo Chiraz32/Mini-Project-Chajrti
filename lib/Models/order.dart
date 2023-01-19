70bef7cc6c084f6d7dc36444484287e7c25edbcc
Your Name <you@example.com>
2023-01-19 23:20:47 +0100

    merging idris work

--- lib/Models/order.dart
+++ lib/Models/order.dart
@@ -1,11 +1,8 @@
 import 'package:chajrti/Models/Plant.dart';
 
 class Order{
-<<<<<<< HEAD
+
  late int id ;
-=======
-  late int id ;
->>>>>>> origin/idris
   late String state;
   late Plant plant;
   late String clientId;
