07787f58680785d4bd81736a6417e4ce706ceb4d
Idris Saddi <saddi.idris@gmail.com>
2023-01-21 17:57:05 +0100

    Client constructor

--- lib/Models/Client.dart
+++ lib/Models/Client.dart
@@ -1,8 +1,16 @@
 class Client {
-  late int id ;
+  late int id;
   late String name;
-  late String image;
+  late String? image;
   late String password;
-  late int phone;
-  Client({required this.id,required this.name,required this.image, required this.password,required this.phone});
+  late int? phone;
+  late String role;
+  Client({
+    required this.id,
+    required this.name,
+    required this.password,
+    required this.role,
+    this.phone,
+    this.image
+  });
 }
